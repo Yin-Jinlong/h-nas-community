14a66b2d6f86d637eb441d347c37b24d65b6554d
YJL <yjl_1567@qq.com>
2024-11-04 22:09:16 +0800

    feat: 传递需要登录，扩大使用范围

--- server/annotation/src/main/kotlin/com/yjl/hnas/annotation/ShouldLogin.kt
+++ server/annotation/src/main/kotlin/com/yjl/hnas/annotation/ShouldLogin.kt
@@ -3,5 +3,10 @@ package com.yjl.hnas.annotation
 /**
  * @author YJL
  */
-@Target(AnnotationTarget.FUNCTION, AnnotationTarget.VALUE_PARAMETER)
+@Target(
+    AnnotationTarget.CLASS,
+    AnnotationTarget.ANNOTATION_CLASS,
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.VALUE_PARAMETER
+)
 annotation class ShouldLogin
