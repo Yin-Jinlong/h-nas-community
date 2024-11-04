474e3945307abcd64f3cc35591706dbe78f7eebe
YJL <yjl_1567@qq.com>
2024-11-04 22:51:35 +0800

    feat: 添加结果缓存

--- server/app/src/main/kotlin/com/yjl/hnas/utils/AnnoUtils.kt
+++ server/app/src/main/kotlin/com/yjl/hnas/utils/AnnoUtils.kt
@@ -4,39 +4,51 @@ import org.springframework.core.MethodParameter
 import org.springframework.core.annotation.AnnotatedMethod
 import kotlin.reflect.KClass
 
+private typealias CacheMap<K> = MutableMap<Pair<K, Class<*>>, Boolean>
+private typealias CacheClassMap = CacheMap<Class<*>>
+
 private val indirectMap = mutableMapOf<Class<*>, Boolean>()
+
+private val annoResultCache: CacheMap<Annotation> = mutableMapOf()
+private val classResultCache: CacheClassMap = mutableMapOf()
+private val methodParameterCache: CacheMap<MethodParameter> = mutableMapOf()
+private val annotatedMethodCache: CacheMap<AnnotatedMethod> = mutableMapOf()
 
 /**
  * 是否包含或间接包含注解
  *
  * 被包含的注解，如果可以间接则必须直接使用[Target]并包含[AnnotationTarget.ANNOTATION_CLASS]
  */
-private fun <A : Annotation> Annotation.hasOrIs(anno: KClass<A>): Boolean {
-    if (anno.java !in indirectMap) {
-        // 获取注解目标
-        val targets: Array<out Target>? = (anno as Any).javaClass.getAnnotationsByType(Target::class.java)
-        // 没有则不可以间接
-        if (targets == null || targets.isEmpty()) {
-            indirectMap[anno.java] = false
-        } else {
-            // 遍历所有，如果任意有
-            indirectMap[anno.java] = targets.any {
-                it.allowedTargets.contains(AnnotationTarget.ANNOTATION_CLASS)
+private fun <A : Annotation> Annotation.hasOrIs(
+    anno: Class<A>
+): Boolean = annoResultCache.getOrPut(this to anno) {
+    this::class == anno || if (indirectMap.getOrPut(anno) { // 缓存
+            // 获取注解目标
+            val targets: Array<out Target>? =
+                (anno as Any).javaClass.getAnnotationsByType(Target::class.java)
+            // 没有则不可以间接
+            if (targets == null || targets.isEmpty()) {
+                false
+            } else {
+                // 遍历所有，如果任意有
+                targets.any {
+                    it.allowedTargets.contains(AnnotationTarget.ANNOTATION_CLASS)
+                }
             }
         }
-    }
-    return this::class == anno ||
-            // 传递
-            if (indirectMap[anno.java]!!) javaClass.annotations.any { it.hasOrIs(anno) }
-            // 直接
-            else javaClass.isAnnotationPresent(anno.java)
+    ) javaClass.annotations.any { it.hasOrIs(anno) } // true
+    // 直接
+    else javaClass.isAnnotationPresent(anno) // false
 }
+
 
 /**
  * 是否包含或间接包含注解
  */
-fun <A : Annotation> Class<*>.hasAnno(anno: KClass<A>): Boolean {
-    return annotations.any { it.hasOrIs(anno) }
+fun <A : Annotation> Class<*>.hasAnno(
+    anno: KClass<A>
+) = classResultCache.getOrPut(this to anno.java) {
+    annotations.any { it.hasOrIs(anno.java) }
 }
 
 /**
@@ -49,8 +61,10 @@ fun <A : Annotation> KClass<*>.hasAnno(anno: KClass<A>) = java.hasAnno(anno)
  *
  * - 参数注解 > 方法注解 > 类注解
  */
-fun <A : Annotation> MethodParameter.hasAnno(anno: KClass<A>): Boolean {
-    return parameter::class.hasAnno(anno) ||
+fun <A : Annotation> MethodParameter.hasAnno(
+    anno: KClass<A>
+) = methodParameterCache.getOrPut(this to anno.java) {
+    parameter::class.hasAnno(anno) ||
             method::class.hasAnno(anno) ||
             declaringClass.hasAnno(anno)
 }
@@ -60,7 +74,9 @@ fun <A : Annotation> MethodParameter.hasAnno(anno: KClass<A>): Boolean {
  *
  * - 方法注解 > 类注解
  */
-fun <A : Annotation> AnnotatedMethod.hasAnno(anno: KClass<A>): Boolean {
-    return method::class.hasAnno(anno) ||
+fun <A : Annotation> AnnotatedMethod.hasAnno(
+    anno: KClass<A>
+) = annotatedMethodCache.getOrPut(this to anno.java) {
+    method::class.hasAnno(anno) ||
             method.declaringClass.hasAnno(anno)
 }
