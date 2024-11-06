b615cb432e81c1a281c7d4ccbb0b6a05c0f7d90f
YJL <yjl_1567@qq.com>
2024-11-06 16:34:10 +0800

    feat: 添加双击事件，取消用户选择

--- web/src/components/file-grid-view/src/file-grid-view.vue
+++ web/src/components/file-grid-view/src/file-grid-view.vue
@@ -1,35 +1,71 @@
 <template>
-    <el-image
-            v-if="info.fileType==='FILE' &&info.preview"
-            :alt="info.path"
-            :src="info.preview"
-            :title="info.path"
-            class="img"
-            fit="cover"
-            loading="lazy">
-        <template #placeholder>
-            <el-skeleton animated data-fill-size>
-                <template #template>
-                    <el-skeleton-item style="width: 8em;height: 8em" variant="image"/>
-                </template>
-            </el-skeleton>
-        </template>
-    </el-image>
-    <el-icon v-else size="8em">
-        <folder v-if="info.fileType=='FOLDER'"/>
-        <unknown-file v-else/>
-    </el-icon>
+    <div class="box"
+         @click="onClick">
+        <el-image
+                v-if="info.fileType==='FILE' &&info.preview"
+                :alt="info.path"
+                :src="info.preview"
+                :title="info.path"
+                class="img"
+                fit="cover"
+                loading="lazy">
+            <template #placeholder>
+                <el-skeleton animated data-fill-size>
+                    <template #template>
+                        <el-skeleton-item style="width: 8em;height: 8em" variant="image"/>
+                    </template>
+                </el-skeleton>
+            </template>
+        </el-image>
+        <el-icon v-else size="100%">
+            <folder v-if="info.fileType=='FOLDER'"/>
+            <unknown-file v-else/>
+        </el-icon>
+    </div>
+
 </template>
 
 <style lang="scss" scoped>
+.box, :deep(.el-icon) {
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-select: none;
+  user-select: none;
 
+  &, & > svg {
+    height: 8em;
+    width: 8em;
+  }
+
+}
 </style>
 
 <script lang="ts" setup>
 import Folder from '@/components/file-grid-view/src/folder.vue'
 import UnknownFile from './unknown-file.vue'
-import {FileGridViewProps} from './props'
+import FileGridViewPropsDefault, {FileGridViewProps} from './props'
 
-const props = defineProps<FileGridViewProps>()
+const props = withDefaults(defineProps<FileGridViewProps>(), FileGridViewPropsDefault)
+const emits = defineEmits({
+    'click': (e: MouseEvent) => {
+    },
+    'dblclick': (last: MouseEvent) => {
+    }
+})
+
+let clickTimeout = 0
+
+function onClick(e: MouseEvent) {
+    if (clickTimeout) {
+        clearTimeout(clickTimeout)
+        clickTimeout = 0
+        emits('dblclick', e)
+    } else {
+        clickTimeout = setTimeout(() => {
+            clickTimeout = 0
+            emits('click', e)
+        }, props.dbClickInterval) as unknown as number
+    }
+}
 
 </script>
--- web/src/components/file-grid-view/src/props.ts
+++ web/src/components/file-grid-view/src/props.ts
@@ -1,5 +1,10 @@
-import {FileInfo} from '@/types/file-info'
-
 export interface FileGridViewProps {
     info: FileInfo
+    dbClickInterval?: number
 }
+
+const FileGridViewPropsDefault = {
+    dbClickInterval: 500
+}
+
+export default FileGridViewPropsDefault
