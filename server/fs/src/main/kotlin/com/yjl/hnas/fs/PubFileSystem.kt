56e29b72aa5d0629a72dd6a1807a4a6fe7e0d2ef
YJL <yjl_1567@qq.com>
2024-11-09 18:15:32 +0800

    fix: 修复路径生成

--- server/fs/src/main/kotlin/com/yjl/hnas/fs/PubFileSystem.kt
+++ server/fs/src/main/kotlin/com/yjl/hnas/fs/PubFileSystem.kt
@@ -9,7 +9,7 @@ class PubFileSystem(
     fsp: PubFileSystemProvider
 ) : VirtualableFileSystem<PubFileSystemProvider, PubFileSystem, PubPath>(fsp) {
     override fun getPath(first: String, vararg more: String): PubPath {
-        return PubPath(this, if (more.isEmpty()) "" else more.joinToString("/", "/"))
+        return PubPath(this, first + if (more.isEmpty()) "" else more.joinToString("/", "/"))
     }
 
     override fun check(path: Path): PubPath {
