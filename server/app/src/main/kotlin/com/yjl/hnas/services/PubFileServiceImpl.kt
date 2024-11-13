13d31c6e7dcbd92f06edff04b7259f8817478b48
YJL <yjl_1567@qq.com>
2024-11-13 18:39:48 +0800

    feat: 实现toVirtualPath

--- server/app/src/main/kotlin/com/yjl/hnas/services/PubFileServiceImpl.kt
+++ server/app/src/main/kotlin/com/yjl/hnas/services/PubFileServiceImpl.kt
@@ -3,11 +3,14 @@ package com.yjl.hnas.services
 import com.yjl.hnas.entity.Uid
 import com.yjl.hnas.entity.VFile
 import com.yjl.hnas.fs.PubPath
+import com.yjl.hnas.fs.VirtualFileSystemProvider
 import com.yjl.hnas.fs.VirtualPath
+import com.yjl.hnas.fs.attr.FileAttribute
 import com.yjl.hnas.mapper.VFileMapper
 import com.yjl.hnas.service.PubFileService
 import com.yjl.hnas.service.VirtualFileService
 import com.yjl.hnas.utils.timestamp
+import org.apache.tika.mime.MediaType
 import org.springframework.stereotype.Service
 import java.nio.file.AccessMode
 import kotlin.io.path.pathString
@@ -19,13 +22,23 @@ import kotlin.io.path.pathString
 class PubFileServiceImpl(
     val vFileMapper: VFileMapper,
     val virtualFileService: VirtualFileService,
+    virtualFileSystemProvider: VirtualFileSystemProvider,
 ) : PubFileService {
+
+    val virtualFileSystem = virtualFileSystemProvider.getFileSystem()
+
     override fun checkAccess(path: PubPath, vararg modes: AccessMode) {
 
     }
 
     override fun toVirtualPath(path: PubPath): VirtualPath {
-        TODO("Not yet implemented")
+        val hash = path.bundleAttrs[FileAttribute.HASH]?.value() as String?
+            ?: throw IllegalArgumentException("path must have hash attr")
+        val type = path.bundleAttrs[FileAttribute.TYPE]?.value() as MediaType?
+            ?: throw IllegalArgumentException("path must have type attr")
+        if (hash.contains("/"))
+            throw IllegalArgumentException("hash must not contain '/'")
+        return virtualFileSystem.getPath("data", type.type, type.subtype, hash)
     }
 
     override fun folderExists(path: PubPath): Boolean {
