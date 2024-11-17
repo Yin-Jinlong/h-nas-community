0ed55329a4b0ea1af8b953fe6bae8f2d4db8bb40
YJL <yjl_1567@qq.com>
2024-11-17 21:50:27 +0800

    del: 去除无用注释

--- server/app/src/main/kotlin/com/yjl/hnas/utils/EntityUtils.kt
+++ server/app/src/main/kotlin/com/yjl/hnas/utils/EntityUtils.kt
@@ -22,34 +22,3 @@ fun VirtualFile.toFileInfo(fileMappingService: FileMappingService): FileInfo {
         }
     )
 }
-
-
-/*fun FileMapping(
-    folderService: FolderService, path: String, res: Resource
-): FileMapping {
-    val md5 = getMd5(res.inputStream)
-    val mime = MediaType.parse(res.inputStream.mimeType)
-    return FileMapping(
-        FileMapping.PK(
-            path.split("/").last(),
-            folder = folderService.getFolderByPath(path)?.id ?: 0,
-        ),
-        dataPath = getDataPath(mime, md5),
-        md5 = md5,
-        type = mime.type,
-        subType = mime.subtype
-    )
-}*/
-
-/*fun FileMapping.toFileInfo(folderService: FolderService): FileInfo {
-    val file = toFile()
-    return FileInfo(
-        folderService.getPath(pk.folder) + pk.name,
-        dataPath,
-        type,
-        if (canPreview()) "/api/thumbnail/${pk.name}"
-        else null,
-        file.lastModified(),
-        file.length()
-    )
-}*/
