26ead68ffd915f5407918dc8b258e22d8b42f2d0
YJL <yjl_1567@qq.com>
2024-11-20 11:35:34 +0800

    feat: 其他请求一律拒绝

--- server/subs/core/src/main/kotlin/com/yjl/hnas/CoreApplication.kt
+++ server/subs/core/src/main/kotlin/com/yjl/hnas/CoreApplication.kt
@@ -1,17 +1,24 @@
 package com.yjl.hnas
 
 import com.google.gson.Gson
+import com.yjl.hnas.error.ErrorCode
+import com.yjl.hnas.resp.RespFactory
 import com.yjl.hnas.token.Token
 import io.github.yinjinlong.spring.boot.annotations.UseWrappedReturnValue
 import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.context.annotation.Lazy
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
 
 @UseWrappedReturnValue
 @EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = ["io.github.yinjinlong.spring.boot", "com.yjl.hnas"])
 @Lazy(false)
+@RestController
 abstract class CoreApplication(
     val gson: Gson
 ) {
@@ -21,4 +28,7 @@ abstract class CoreApplication(
         Token.init(gson)
     }
 
+    @RequestMapping("/**")
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    fun forbidden() = RespFactory.clientError(ErrorCode.NO_PERMISSION.error)
 }
