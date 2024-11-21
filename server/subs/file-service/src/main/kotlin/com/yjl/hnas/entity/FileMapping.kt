48d506390621a670a1a05cf1c11d18f56be3632d
YJL <yjl_1567@qq.com>
2024-11-21 13:53:38 +0800

    fix: 修复nullable

--- server/subs/file-service/src/main/kotlin/com/yjl/hnas/entity/FileMapping.kt
+++ server/subs/file-service/src/main/kotlin/com/yjl/hnas/entity/FileMapping.kt
@@ -20,19 +20,19 @@ data class FileMapping(
     @Comment("文件hash, base64<<sha256<<data")
     override var hash: String = "",
 
-    @Column(length = IVFile.PATH_LENGTH)
+    @Column(length = IVFile.PATH_LENGTH, nullable = false)
     @Comment("文件路径")
     override var dataPath: String = "",
 
-    @Column(length = 32)
+    @Column(length = 32, nullable = false)
     @Comment("类型")
     override var type: String = "",
 
-    @Column(length = 32)
+    @Column(length = 32, nullable = false)
     @Comment("子类型")
     override var subType: String = "",
 
-    @Column(columnDefinition = "bigint default(-1)")
+    @Column(columnDefinition = "bigint default(-1)", nullable = false)
     @Comment("文件大小")
     override var size: Long = -1
 ) : IFileMapping
