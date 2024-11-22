18f2d369b05492238be33229798b0f8044ae3fd9
YJL <yjl_1567@qq.com>
2024-11-22 19:46:42 +0800

    fix: 修复文件信息显示

--- web/src/components/file-info-dialog/src/file-info-dialog.vue
+++ web/src/components/file-info-dialog/src/file-info-dialog.vue
@@ -1,25 +1,26 @@
 <template>
-    <el-dialog v-model="modelValue">
+    <el-dialog v-model="modelValue"
+               style="--el-dialog-width: max-content;max-width: 90%">
         <template #header>
             <h3>{{ info.name ?? '' }}</h3>
         </template>
         <template #default>
             <div data-flex>
-                <file-grid-view v-if="modelValue"
-                                v-model="extra"
-                                :info="info"/>
+                <file-grid-view
+                        v-if="modelValue"
+                        v-model="extra"
+                        :info="info"
+                        style="flex: 0 0 auto"/>
                 <table style="margin-left: 1em">
                     <tbody>
                     <tr v-for="r in infoTable">
-                        <td>
-                            <div data-fill-size
-                                 data-flex-center
-                                 style="justify-content: end;padding: 0.25em 0.2em 0.25em 0;">
-                                <label>{{ r.label }}：</label>
-                            </div>
+                        <td class="label"
+                            data-fill-size
+                            data-flex-center>
+                            <label>{{ r.label }}：</label>
                         </td>
-                        <td>
-                            <span>{{ r.value }}</span>
+                        <td style="word-wrap: anywhere">
+                            {{ r.value }}
                         </td>
                     </tr>
                     </tbody>
@@ -30,7 +31,17 @@
 </template>
 
 <style lang="scss" scoped>
+.label {
+  display: flex;
+  justify-content: end;
+  min-width: 5em;
+  padding: 0.25em 0.2em 0.25em 0;
+  width: 100%;
 
+  & > label {
+    flex: 0 0 auto;
+  }
+}
 </style>
 
 <script lang="ts" setup>
