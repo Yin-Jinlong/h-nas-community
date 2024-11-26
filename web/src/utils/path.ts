a39dacbb8e7f5eee7e3b19a6d68d410dcb968b78
YJL <yjl_1567@qq.com>
2024-11-26 21:43:27 +0800

    fix: 修复路径首/

--- web/src/utils/path.ts
+++ web/src/utils/path.ts
@@ -1,6 +1,7 @@
 export function subPath(dir: string | string[], sub: string) {
-    if (Array.isArray(dir))
-        dir = dir.join('/')
+    if (Array.isArray(dir)) {
+        dir = (dir.length ? '/' : '') + dir.join('/')
+    }
     if (dir.endsWith('/'))
         return dir + sub
     return dir + '/' + sub
