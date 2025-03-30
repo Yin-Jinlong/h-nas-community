e7fb94ca4aaeeaf46d29b4cb53058b72043eb587
YJL <yjl_1567@qq.com>
2025-03-30 17:00:18 +0800

    feat: 添加扫描登录按钮

--- client/lib/pages/login_logon.dart
+++ client/lib/pages/login_logon.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:universal_platform/universal_platform.dart';
 
 import '../generated/l10n.dart';
 
@@ -24,59 +25,7 @@ class _LogInOnPageState extends State<LogInOnPage> {
               child: Card(
                 child: Padding(
                   padding: EdgeInsets.all(12),
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          BackButton(),
-                          Text(
-                            S.current.login,
-                            style: TextTheme.of(context).headlineSmall,
-                          ),
-                        ],
-                      ),
-                      Hero(
-                        tag: 'login',
-                        child: const Icon(Icons.person, size: 50),
-                      ),
-                      Expanded(
-                        child: Padding(
-                          padding: EdgeInsets.all(8),
-                          child: TextField(
-                            decoration: InputDecoration(
-                              labelText: S.current.username,
-                              hintText: '${S.current.username}/id',
-                              hintStyle: TextStyle(color: Colors.grey),
-                              border: OutlineInputBorder(),
-                              prefixIcon: Icon(Icons.person),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        child: Padding(
-                          padding: EdgeInsets.all(8),
-                          child: TextField(
-                            obscureText: true,
-                            decoration: InputDecoration(
-                              labelText: S.current.password,
-                              border: OutlineInputBorder(),
-                              prefixIcon: Icon(Icons.lock),
-                            ),
-                          ),
-                        ),
-                      ),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: ColorScheme.of(context).primary,
-                          foregroundColor: ColorScheme.of(context).onPrimary,
-                          minimumSize: Size(double.infinity, 50),
-                        ),
-                        onPressed: () {},
-                        child: Text(S.current.login),
-                      ),
-                    ],
-                  ),
+                  child: Stack(children: [_loginColumn(context), _qrLayout()]),
                 ),
               ),
             ),
@@ -84,5 +33,92 @@ class _LogInOnPageState extends State<LogInOnPage> {
         ),
       ),
     );
+  }
+}
+
+Widget _loginColumn(BuildContext context) {
+  return Column(
+    children: [
+      Row(
+        children: [
+          BackButton(),
+          Text(S.current.login, style: TextTheme.of(context).headlineSmall),
+        ],
+      ),
+      Hero(tag: 'login', child: const Icon(Icons.person, size: 50)),
+      Expanded(
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: TextField(
+            decoration: InputDecoration(
+              labelText: S.current.username,
+              hintText: '${S.current.username}/id',
+              hintStyle: TextStyle(color: Colors.grey),
+              border: OutlineInputBorder(),
+              prefixIcon: Icon(Icons.person),
+            ),
+          ),
+        ),
+      ),
+      Expanded(
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: S.current.password,
+              border: OutlineInputBorder(),
+              prefixIcon: Icon(Icons.lock),
+            ),
+          ),
+        ),
+      ),
+      ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: ColorScheme.of(context).primary,
+          foregroundColor: ColorScheme.of(context).onPrimary,
+          minimumSize: Size(double.infinity, 50),
+        ),
+        onPressed: () {},
+        child: Text(S.current.login),
+      ),
+    ],
+  );
+}
+
+Widget _qrLayout() {
+  if (UniversalPlatform.isDesktopOrWeb) {
+    return Align(
+      alignment: Alignment.topRight,
+      child: Tooltip(
+        message: S.current.scan_login,
+        child: ClipPath(
+          clipper: _QRClipper(),
+          child: TextButton(
+            onPressed: () {},
+            child: Icon(Icons.qr_code, size: 60),
+          ),
+        ),
+      ),
+    );
+  } else {
+    return Container();
+  }
+}
+
+class _QRClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    // 右上三角
+    return Path()
+      ..moveTo(0, 0)
+      ..lineTo(size.width, 0)
+      ..lineTo(size.width, size.height)
+      ..close();
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return true;
   }
 }
