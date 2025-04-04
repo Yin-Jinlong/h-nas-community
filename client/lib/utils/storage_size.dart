7f333170227ee31ef63f65fd338688da0093f18d
YJL <yjl_1567@qq.com>
2025-04-04 14:10:14 +0800

    fix: 修复小数位显示

--- client/lib/utils/storage_size.dart
+++ client/lib/utils/storage_size.dart
@@ -6,12 +6,13 @@ extension StorageSize on int {
   }
 
   String autoHuman({int fixed = 2, double carryFactor = 0.9}) {
-    var n = this;
+    var n = this, r = 0.0;
     var ui = 0;
     while (n / 1024.0 > carryFactor) {
+      r = n / 1024;
       n ~/= 1024;
       ui++;
     }
-    return '${n.toStringAsFixed(fixed)}${_uints[ui]}B';
+    return '${r.toStringAsFixed(fixed)}${_uints[ui]}B';
   }
 }
