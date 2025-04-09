39ab0dc822f56d7e6f56b872cec494ca0bf9dc85
YJL <yjl_1567@qq.com>
2025-04-09 16:16:27 +0800

    feat: 美化播放页布局

--- client/lib/pages/audio_player/audio_player.dart
+++ client/lib/pages/audio_player/audio_player.dart
@@ -1,7 +1,10 @@
+import 'dart:ui';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:h_nas/components/record_view.dart';
 import 'package:h_nas/global.dart';
+import 'package:h_nas/media/media_player.dart';
 import 'package:h_nas/utils/time_utils.dart';
 
 import '../../generated/l10n.dart';
@@ -17,10 +20,13 @@ class AudioPlayerPage extends StatefulWidget {
 class _AudioPlayerPageState extends State<AudioPlayerPage>
     with TickerProviderStateMixin {
   late final AnimationController _playPauseController;
+  late final MediaPlayer player;
 
   @override
   void initState() {
     super.initState();
+
+    player = Global.player;
 
     _playPauseController = AnimationController(
       value: Global.player.playing ? 1 : 0,
@@ -28,9 +34,9 @@ class _AudioPlayerPageState extends State<AudioPlayerPage>
       duration: durationFast,
     );
 
-    Global.player.position.addListener(_render);
+    player.position.addListener(_render);
 
-    Global.player.playState.addListener(_onPlay);
+    player.playState.addListener(_onPlay);
   }
 
   _render() {
@@ -39,7 +45,7 @@ class _AudioPlayerPageState extends State<AudioPlayerPage>
 
   _onPlay() {
     setState(() {
-      if (Global.player.playing) {
+      if (player.playing) {
         _playPauseController.forward();
       } else {
         _playPauseController.reverse();
@@ -49,8 +55,8 @@ class _AudioPlayerPageState extends State<AudioPlayerPage>
 
   @override
   void dispose() {
-    Global.player.position.removeListener(_render);
-    Global.player.playState.removeListener(_onPlay);
+    player.position.removeListener(_render);
+    player.playState.removeListener(_onPlay);
 
     _playPauseController.dispose();
 
@@ -58,7 +64,6 @@ class _AudioPlayerPageState extends State<AudioPlayerPage>
   }
 
   List<Widget> _controllers() {
-    final player = Global.player;
     return [
       Hero(
         tag: 'play_pause',
@@ -77,68 +82,126 @@ class _AudioPlayerPageState extends State<AudioPlayerPage>
     ];
   }
 
+  Widget _cover() {
+    return Hero(
+      tag: 'audio_cover',
+      child: RecordView(
+        rotate: player.playing,
+        size: MediaQuery.of(context).size.width * 0.5,
+        child: CachedNetworkImage(
+          imageUrl: FileAPIURL.publicAudioCover(player.audioInfo.value!.path),
+          fit: BoxFit.cover,
+          errorWidget: (context, error, stackTrace) {
+            return Icon(Icons.broken_image);
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _info(BuildContext context) {
+    final info = player.audioInfo.value;
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 12),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            info?.title ?? '?',
+            style: TextTheme.of(
+              context,
+            ).headlineSmall?.copyWith(color: Colors.white),
+          ),
+          Text(
+            info?.artists ?? '?',
+            style: TextTheme.of(
+              context,
+            ).bodyLarge?.copyWith(color: Colors.white),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _progressInfo() {
+    return Row(
+      children: [
+        Padding(
+          padding: EdgeInsets.only(left: 6),
+          child: Text(player.position.value.shortTimeStr),
+        ),
+        Expanded(
+          child: Slider(
+            value: player.progress ?? 0,
+            inactiveColor: Colors.grey.withValues(alpha: 0.3),
+            onChanged: (value) {
+              player.seek(
+                Duration(seconds: (player.duration.value * value).toInt()),
+              );
+              setState(() {});
+            },
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(right: 6),
+          child: Text(player.duration.value.shortTimeStr),
+        ),
+      ],
+    );
+  }
+
+  Widget _content(BuildContext context) {
+    return Column(
+      children: [
+        Row(children: [BackButton()]),
+        Expanded(child: Center(child: _cover())),
+        _info(context),
+        _progressInfo(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: _controllers(),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final player = Global.player;
-    final audioInfo = player.audioInfo.value;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          '${audioInfo?.title} - ${audioInfo?.album} - ${audioInfo?.artists}',
-        ),
-      ),
       body: Stack(
         children: [
-          Column(
-            children: [
-              Hero(
-                tag: 'audio_cover',
-                child: RecordView(
-                  rotate: player.playing,
-                  size: 200,
-                  child: CachedNetworkImage(
-                    imageUrl: FileAPIURL.publicAudioCover(
-                      player.audioInfo.value!.path,
-                    ),
-                    fit: BoxFit.cover,
-                    errorWidget: (context, error, stackTrace) {
-                      return Icon(Icons.broken_image);
-                    },
-                  ),
-                ),
+          CachedNetworkImage(
+            imageUrl:
+                player.audioInfo.value != null
+                    ? FileAPIURL.publicAudioCover(player.audioInfo.value!.path)
+                    : '',
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+            errorWidget: (context, error, stackTrace) {
+              return Container();
+            },
+          ),
+          BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 50, sigmaY: 50),
+            child: Container(color: Colors.black.withValues(alpha: 0.3)),
+          ),
+          IconTheme(
+            data: IconThemeData(
+              color:
+                  HSVColor.fromColor(
+                    ColorScheme.of(context).primary,
+                  ).withSaturation(0.1).withValue(1).toColor(),
+            ),
+            child: DefaultTextStyle(
+              style: TextStyle(color: Colors.white),
+              child: Padding(
+                padding: EdgeInsets.all(8),
+                child: _content(context),
               ),
-              Row(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(left: 6),
-                    child: Text(player.position.value.shortTimeStr),
-                  ),
-                  Expanded(
-                    child: Slider(
-                      value: player.progress ?? 0,
-                      inactiveColor: Colors.grey.withValues(alpha: 0.3),
-                      onChanged: (value) {
-                        player.seek(
-                          Duration(
-                            seconds: (player.duration.value * value).toInt(),
-                          ),
-                        );
-                        setState(() {});
-                      },
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(right: 6),
-                    child: Text(player.duration.value.shortTimeStr),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: _controllers(),
-              ),
-            ],
+            ),
           ),
         ],
       ),
