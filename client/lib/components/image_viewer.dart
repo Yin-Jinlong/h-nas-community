87dc9b759a707c0c57deb5a365ea9135888c8b11
YJL <yjl_1567@qq.com>
2025-04-10 22:27:32 +0800

    fix: 修复图片预览控制点击颜色

--- client/lib/components/image_viewer.dart
+++ client/lib/components/image_viewer.dart
@@ -365,6 +365,8 @@ class _ImageViewerControllerState extends State<_ImageViewerController> {
   @override
   Widget build(BuildContext context) {
     return InkWell(
+      splashColor: Colors.transparent,
+      highlightColor: Colors.transparent,
       onTap: () {},
       onHover: (v) {
         setState(() {
