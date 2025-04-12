af4681f87e374dc33a02d2ce63da2caeeb6f04aa
YJL <yjl_1567@qq.com>
2025-04-12 14:54:54 +0800

    feat: 添加登录注册背景

--- client/lib/pages/login_logon/login_logon.dart
+++ client/lib/pages/login_logon/login_logon.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:h_nas/components/tab_page.dart';
 import 'package:h_nas/model/user_model.dart';
@@ -24,76 +27,115 @@ class LogInOnPage extends StatefulWidget {
 
 class _LogInOnPageState extends State<LogInOnPage> {
   var pageIndex = 0;
+  final List<_BackgroundItem> _bgItems = [];
+
+  @override
+  void initState() {
+    super.initState();
+    final random = Random();
+
+    const count = 6;
+    const offCount = 10;
+
+    for (var i = 0; i < count; i++) {
+      _bgItems.add(
+        _BackgroundItem(
+          pos: Offset(random.nextDouble(), random.nextDouble()),
+          radius: random.nextDouble(),
+          colorOff: (random.nextInt(offCount - 1) + 1) * 360 / offCount,
+        ),
+      );
+    }
+  }
+
+  Widget _background(BuildContext context) {
+    return SizedBox.expand(
+      child: CustomPaint(
+        painter: _BackgroundPainter(
+          color: HSVColor.fromColor(ColorScheme.of(context).primary),
+          items: _bgItems,
+        ),
+      ),
+    );
+  }
+
+  Widget _content() {
+    return Card(
+      child: Padding(
+        padding: EdgeInsets.all(12),
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                Row(
+                  children: [
+                    IconButton(
+                      tooltip: S.current.back,
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      icon: Hero(
+                        tag: 'menu_back',
+                        child: Icon(Icons.arrow_back),
+                      ),
+                    ),
+                    Text(
+                      pageIndex == 0 ? S.current.login : S.current.logon,
+                      style: TextTheme.of(context).headlineSmall,
+                    ),
+                  ],
+                ),
+                Hero(tag: 'login', child: const Icon(Icons.person, size: 50)),
+                TabPage(
+                  index: pageIndex,
+                  children: [
+                    _LoginWidget(
+                      onGotoLogon: () {
+                        setState(() {
+                          pageIndex = 1;
+                        });
+                      },
+                    ),
+                    _LogonWidget(
+                      onGotoLogin: () {
+                        setState(() {
+                          pageIndex = 0;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            pageIndex == 0 ? _qrLayout() : Container(),
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorScheme.of(context).secondary,
-      body: Center(
-        child: IntrinsicWidth(
-          child: IntrinsicHeight(
-            child: ConstrainedBox(
-              constraints: BoxConstraints(minWidth: 300),
-              child: Card(
-                child: Padding(
-                  padding: EdgeInsets.all(12),
-                  child: Stack(
-                    children: [
-                      Column(
-                        children: [
-                          Row(
-                            children: [
-                              IconButton(
-                                tooltip: S.current.back,
-                                onPressed: () {
-                                  Navigator.of(context).pop();
-                                },
-                                icon: Hero(
-                                  tag: 'menu_back',
-                                  child: Icon(Icons.arrow_back),
-                                ),
-                              ),
-                              Text(
-                                pageIndex == 0
-                                    ? S.current.login
-                                    : S.current.logon,
-                                style: TextTheme.of(context).headlineSmall,
-                              ),
-                            ],
-                          ),
-                          Hero(
-                            tag: 'login',
-                            child: const Icon(Icons.person, size: 50),
-                          ),
-                          TabPage(
-                            index: pageIndex,
-                            children: [
-                              _LoginWidget(
-                                onGotoLogon: () {
-                                  setState(() {
-                                    pageIndex = 1;
-                                  });
-                                },
-                              ),
-                              _LogonWidget(
-                                onGotoLogin: () {
-                                  setState(() {
-                                    pageIndex = 0;
-                                  });
-                                },
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                      pageIndex == 0 ? _qrLayout() : Container(),
-                    ],
-                  ),
+      body: Stack(
+        children: [
+          _background(context),
+          BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 150, sigmaY: 150),
+            child: Container(),
+          ),
+          Center(
+            child: IntrinsicWidth(
+              child: IntrinsicHeight(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(minWidth: 300),
+                  child: _content(),
                 ),
               ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
@@ -134,4 +176,56 @@ class _QRClipper extends CustomClipper<Path> {
   bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
     return true;
   }
+}
+
+class _BackgroundItem {
+  Offset pos;
+  double radius;
+  double colorOff;
+  late Paint _paint;
+
+  _BackgroundItem({
+    required this.pos,
+    required this.radius,
+    required this.colorOff,
+  }) {
+    _paint =
+        Paint()
+          ..isAntiAlias = true
+          ..style = PaintingStyle.fill;
+  }
+
+  void draw(
+    Canvas canvas,
+    HSVColor color,
+    double width,
+    double height,
+    double r,
+  ) {
+    _paint.color = color.withHue((color.hue + colorOff) % 360).toColor();
+    canvas.drawCircle(
+      Offset(pos.dx * width, pos.dy * height),
+      radius * r,
+      _paint,
+    );
+  }
+}
+
+class _BackgroundPainter extends CustomPainter {
+  final HSVColor color;
+  final List<_BackgroundItem> items;
+
+  _BackgroundPainter({required this.color, required this.items});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final maxR = sqrt(size.width * size.width + size.height * size.height) / 3;
+
+    for (final item in items) {
+      item.draw(canvas, color, size.width, size.height, maxR);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
