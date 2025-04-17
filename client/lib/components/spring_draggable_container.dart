d2f6f9b1e539448267159ec9814af4de1f0f838e
YJL <yjl_1567@qq.com>
2025-04-17 16:19:21 +0800

    feat: 添加SpringDraggableContainer事件消费配置

--- client/lib/components/spring_draggable_container.dart
+++ client/lib/components/spring_draggable_container.dart
@@ -2,11 +2,18 @@ import 'package:flutter/cupertino.dart';
 
 class SpringDraggableContainer extends StatefulWidget {
   final Duration duration;
+
+  /// 只监听，不消费事件
+  /// 如果为true，子组件仍能响应pan事件
+  /// 否则该组件会消费pan事件
+  /// 默认为[false]
+  final bool onlyListen;
   final Widget child;
 
   const SpringDraggableContainer({
     super.key,
     required this.child,
+    this.onlyListen = false,
     this.duration = const Duration(milliseconds: 400),
   });
 
@@ -30,6 +37,20 @@ class _SpringContainerState extends State<SpringDraggableContainer>
     _animation = _controller.drive(CurveTween(curve: Curves.elasticIn));
   }
 
+  void _init(Offset pos) {
+    setState(() {
+      _controller.value = 1;
+      _down = pos;
+      _off = Offset(0, 0);
+    });
+  }
+
+  void _update(Offset pos) {
+    setState(() {
+      _off = pos - _down;
+    });
+  }
+
   void _start() {
     _controller.reverse();
   }
@@ -48,27 +69,37 @@ class _SpringContainerState extends State<SpringDraggableContainer>
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onPanDown: (event) {
-        setState(() {
-          _controller.value = 1;
-          _down = event.localPosition;
-          _off = Offset(0, 0);
-        });
-      },
-      onPanUpdate: (event) {
-        setState(() {
-          _off = event.localPosition - _down;
-        });
-      },
-      onPanEnd: (details) {
-        _start();
-      },
-      onPanCancel: _start,
-      child: Transform.translate(
-        offset: _off * _animation.value,
-        child: widget.child,
-      ),
-    );
+    return widget.onlyListen
+        ? Listener(
+          onPointerDown: (event) {
+            _init(event.localPosition);
+          },
+          onPointerMove: (event) {
+            _update(event.localPosition);
+          },
+          onPointerUp: (event) {
+            _start();
+          },
+          child: Transform.translate(
+            offset: _off * _animation.value,
+            child: widget.child,
+          ),
+        )
+        : GestureDetector(
+          onPanDown: (event) {
+            _init(event.localPosition);
+          },
+          onPanUpdate: (event) {
+            _update(event.localPosition);
+          },
+          onPanEnd: (details) {
+            _start();
+          },
+          onPanCancel: _start,
+          child: Transform.translate(
+            offset: _off * _animation.value,
+            child: widget.child,
+          ),
+        );
   }
 }
