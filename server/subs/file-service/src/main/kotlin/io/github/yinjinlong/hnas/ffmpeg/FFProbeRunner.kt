d96e480ed027b4f5e5b77e5faa98934d885de22b
YJL <yjl_1567@qq.com>
2025-04-27 19:44:52 +0800

    feat: 使用内置ffprobe

--- server/subs/file-service/src/main/kotlin/io/github/yinjinlong/hnas/ffmpeg/FFProbeRunner.kt
+++ server/subs/file-service/src/main/kotlin/io/github/yinjinlong/hnas/ffmpeg/FFProbeRunner.kt
@@ -1,11 +1,63 @@
 package io.github.yinjinlong.hnas.ffmpeg
 
 import com.google.gson.GsonBuilder
+import io.github.yinjinlong.hnas.utils.mkParent
+import org.bytedeco.ffmpeg.ffmpeg
+import java.io.File
+import java.net.JarURLConnection
 
 /**
  * @author YJL
  */
-object FFProbeRunner : CommandLineRunner("ffprobe") {
+object FFProbeRunner : CommandLineRunner("bin/ffprobe") {
+
+    private val platform: String by lazy {
+        val os = System.getProperty("os.name").lowercase()
+        when {
+            os.startsWith("windows") -> "windows"
+            os.startsWith("mac os x") -> "macosx"
+            os.contains("linux") -> "linux"
+            else -> throw UnsupportedOperationException("Unsupported operating system: $os")
+        }
+    }
+
+    private val arch: String by lazy {
+        val arch = System.getProperty("os.arch").lowercase()
+        when {
+            arch.startsWith("arm") -> "arm"
+            arch.startsWith("x86") -> "x86"
+            arch.startsWith("amd64") -> "x86_64"
+            else -> throw UnsupportedOperationException("Unsupported architecture: $arch")
+        }
+    }
+
+    private fun extractProbe() {
+        val file = File("bin/ffprobe" + if (platform == "windows") ".exe" else "")
+        if (file.exists())
+            return
+        file.mkParent()
+
+        val dir = "org/bytedeco/ffmpeg/$platform-$arch"
+
+        val loader = ffmpeg::class.java.classLoader
+        val url = loader.getResource(dir)
+            ?: throw IllegalArgumentException("$dir not found")
+
+        val bin = File("./bin")
+        bin.mkParent()
+
+        val i = url.openConnection() as JarURLConnection
+        for (e in i.jarFile.entries()) {
+            if (e.name.endsWith("/") || e.size == 0L || !e.name.startsWith(dir) || e.name.contains("jni"))
+                continue
+            val name = e.name.split("/").last()
+            bin.resolve(name).also { f ->
+                f.outputStream().use { out ->
+                    i.jarFile.getInputStream(e).use { it.copyTo(out) }
+                }
+            }.setExecutable(true, false)
+        }
+    }
 
     val args = listOf(
         "-v", "quiet",
@@ -25,10 +77,12 @@ object FFProbeRunner : CommandLineRunner("ffprobe") {
      * 获取文媒体信息
      */
     fun probe(file: String): FFProbeResult {
+        extractProbe()
         return gson.fromJson(
             run(*args.toTypedArray(), file).decodeToString(),
             FFProbeResult::class.java
         )
     }
+
 
 }
