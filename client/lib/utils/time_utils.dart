d8f570a3d570d2686efed9332ce2433589fcb7b6
YJL <yjl_1567@qq.com>
2025-05-19 14:40:57 +0800

    opt: 优化命名

--- client/lib/utils/time_utils.dart
+++ client/lib/utils/time_utils.dart
@@ -1,10 +1,10 @@
-String _2(int n) => n < 10 ? '0$n' : '$n';
+String _w2(int n) => n < 10 ? '0$n' : '$n';
 
 extension TImeExt on num {
   String get shortTimeStr {
     var t = toInt();
     final s = t % 60;
     t ~/= 60;
-    return '${_2(t)}:${_2(s)}';
+    return '${_w2(t)}:${_w2(s)}';
   }
 }
